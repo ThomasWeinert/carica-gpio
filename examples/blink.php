633f98afce9732ddc97e9880679dc83dd203b5d3
Thomas Weinert <thomas@weinert.info>
2016-07-16 17:23:06 +0200

    Make the GpioTools the default in the examples

--- examples/blink.php
+++ examples/blink.php
@@ -5,8 +5,8 @@ require(__DIR__.'/../vendor/autoload.php');
 $loop = \Carica\Io\Event\Loop\Factory::get();
 
 $board = new \Carica\Gpio\Boards\RaspberryPiB(
-  new \Carica\Gpio\Commands\Sysfs()
-  //new \Carica\Gpio\Commands\GpioTools()
+  //new \Carica\Gpio\Commands\Sysfs()
+  new \Carica\Gpio\Commands\GpioTools()
 );
 /** @var Carica\Io\Device\Pin $pin */
 $pin = $board->pins[17];
