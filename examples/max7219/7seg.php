13e3dab195bbd50ef5a8ebce6ffa7344b9afae2d
Thomas Weinert <thomas@weinert.info>
2016-07-19 08:36:33 +0200

    added generic shiftout using the pins object (slow, but working)

---
+++ examples/max7219/7seg.php
@@ -0,0 +1,48 @@
+<?php
+$board = require(__DIR__.'/../bootstrap.php');
+
+use Carica\Io;
+
+$loop = Io\Event\Loop\Factory::get();
+
+$digits = 8;
+$maximum = pow(10, $digits) - 0;
+
+$max = new Carica\Gpio\ShiftOut(
+  $board->pins[38],
+  $board->pins[40],
+  $board->pins[36]
+);
+$max->write([0x0F, 0x01]);
+sleep(1);
+$max->write([0x0F, 0x00]);
+// Enable mode B
+$max->write([0x09, 0xFF]);
+// Use max intensity
+$max->write([0x0A, 0xFF]);
+// Only scan eight digits
+$max->write([0x0B, $digits - 1]);
+// Turn on chip
+$max->write([0x0C, 0x01]);
+
+$loop->setInterval(
+  function () use ($max, $maximum) {
+    static $number = 0;
+
+    if (--$number < 0) {
+      $number = $maximum;
+    }
+
+    $string = str_pad($number, 8, ' ',STR_PAD_LEFT);
+    echo $string, "\n";
+
+    $length = strlen($string);
+    for($i = 0; $i < $length; $i++) {
+      $digit = $string[$i] === ' ' ? 0 : (int)$string[$i];
+      $max->write([8 - $i, $digit]);
+    }
+  },
+  200
+);
+
+$loop->run();
---
+++ src/ShiftOut.php
@@ -0,0 +1,81 @@
+<?php
+namespace Carica\Gpio {
+
+  use Carica\Io\Device;
+
+  class ShiftOut implements Device\ShiftOut {
+
+    /**
+     * @var Pin
+     */
+    private $_latchPin = NULL;
+    /**
+     * @var Pin
+     */
+    private $_clockPin = NULL;
+    /**
+     * @var Pin
+     */
+    private $_dataPin = NULL;
+
+    private $_highLatch = FALSE;
+
+    public function __construct(Pin $latch, Pin $clock, Pin $data, $highLatch = FALSE) {
+      $this->_latchPin = $latch;
+      $this->_clockPin = $clock;
+      $this->_dataPin = $data;
+      $this->_highLatch = (bool)$highLatch;
+    }
+
+    public function write($data, $isBigEndian = TRUE) {
+      $this->begin();
+      $this->transfer($data, $isBigEndian);
+      $this->end();
+    }
+
+    /**
+     * Begin transfer (put the latch pin to low)
+     */
+    public function begin() {
+      $this->_latchPin->setMode(Pin::MODE_OUTPUT);
+      $this->_clockPin->setMode(Pin::MODE_OUTPUT);
+      $this->_dataPin->setMode(Pin::MODE_OUTPUT);
+      $this->_latchPin->setDigital($this->_highLatch);
+    }
+
+    /**
+     * Begin transfer (put the latch pin to high)
+     */
+    public function end() {
+      $this->_latchPin->setDigital(!$this->_highLatch);
+    }
+
+    public function transfer($data, $isBigEndian = TRUE) {
+      $write = function($value, $mask) {
+        $this->_clockPin->setDigital(FALSE);
+        $this->_dataPin->setDigital($value & $mask);
+        $this->_clockPin->setDigital(TRUE);
+      };
+
+      if (is_string($data)) {
+        $values = array_slice(unpack("C*", "\0".$value), 1);
+      } elseif (is_array($data)) {
+        $values = $data;
+      } else {
+        $values = array((int)$data);
+      }
+
+      foreach ($values as $value) {
+        if ($isBigEndian) {
+          for ($mask = 128; $mask > 0; $mask = $mask >> 1) {
+            $write($value, $mask);
+          }
+        } else {
+          for ($mask = 0; $mask < 128; $mask = $mask << 1) {
+            $write($value, $mask);
+          }
+        }
+      }
+    }
+  }
+}
